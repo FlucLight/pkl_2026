3be21666ce3b0d8f16f1b409a0e3b0953826cfa9
Zynx <rahmatalfarizi302@gmail.com>
2026-08-04 11:08:28 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TugasController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -41,5 +42,20 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', fn() => view('dashboard'))->name('dashboard');
+
+Route::get('/', [TugasController::class, 'index'])->name('tugas.index');
+Route::post('/tugas', [TugasController::class, 'store'])->name('tugas.store');
+Route::put('/tugas/{id}', [TugasController::class, 'update'])->name('tugas.update');
+Route::delete('/tugas/{id}', [TugasController::class, 'destroy'])->name('tugas.destroy');
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
 

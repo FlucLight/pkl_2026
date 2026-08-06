df1d76595bf5937a8d30ac097ac3d99a7318c008
2ndcois <hibatulq2411@gmail.com>
2026-08-06 08:36:41 +0700

    ini udah ada nilai

--- fastAPI2/models/kumpul.py
+++ fastAPI2/models/kumpul.py
@@ -6,9 +6,11 @@ class kumpul(SQLModel, table=True):
     id_kumpul: Optional[int] = Field(default=None, primary_key=True)
     id_tugas: int = Field(index=True)
     nama_mahasiswa: str = Field(index=True)
+    nilai_mahasiswa: float = Field(default=0.0, index=True)
     tanggal_kumpul: datetime = Field(...)
 
 class kumpul_update(SQLModel):
     id_tugas: int 
     nama_mahasiswa: str 
+    nilai_mahasiswa: float
     tanggal_kumpul: datetime 
